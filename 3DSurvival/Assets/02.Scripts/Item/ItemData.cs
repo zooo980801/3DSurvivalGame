d18e75d540824eae8ccda7630045eab0c8024a50
zooo980801 <zooo980801@gmail.com>
2025-05-26 16:37:07 +0900

    Fix_Enum

--- 3DSurvival/Assets/02.Scripts/Item/ItemData.cs
+++ 3DSurvival/Assets/02.Scripts/Item/ItemData.cs
@@ -5,13 +5,13 @@
 // ItemData : 아이템 설계 정보
 // SaveIem : 세이브시 사용하는 ID+ 수량 저장 전용
 // SaveData : 전체 게임 상태 저장을 위한 마스터클래스
-public enum ItemType { Resource, Equipable, Consumable }
-public enum ConsumableType { Hunger, Health }
+public enum ITEMTYPE { Resource, Equipable, Consumable }
+public enum CONSUMABLETYPE { Hunger, Health } 
 
 [System.Serializable]
 public class ItemDataConsumable
 {
-    public ConsumableType type;
+    public CONSUMABLETYPE type;
     public float value;
 }
 
@@ -22,7 +22,7 @@ public class ItemData : ScriptableObject
     public string id; // 저장 시 참조할 고유 ID
     public string displayName;
     public string description;
-    public ItemType type;
+    public ITEMTYPE type;
     public Sprite icon;
     public GameObject dropPrefab;
 

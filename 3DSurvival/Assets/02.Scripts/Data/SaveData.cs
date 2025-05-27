11efab17951bd3cab18e3d7ad9b5644547e8dc9b
zooo980801 <zooo980801@gmail.com>
2025-05-27 19:16:02 +0900

    Refactor_Status/ Refactor_Data

--- 3DSurvival/Assets/02.Scripts/Data/SaveData.cs
+++ 3DSurvival/Assets/02.Scripts/Data/SaveData.cs
@@ -4,10 +4,13 @@
 public class SaveData // ← 저장용 클래스 명칭
 {
     public int level;
-    public int hp;
-    public int stamina;
     public int exp;
     public int gold;
+
+    public SaveStatusData health;
+    public SaveStatusData stamina;
+    public SaveStatusData hunger;
+    public SaveStatusData thirst;
 
     public List<SaveItem> inventory = new List<SaveItem>();
     public SaveEquipment equipped;
@@ -15,6 +18,14 @@ public class SaveData // ← 저장용 클래스 명칭
 }
 
 [System.Serializable]
+
+public class SaveStatusData
+{
+    public float curValue;
+    public float maxValue;
+    public float passiveValue;
+}
+
 public class SaveItem // 저장용 아이템
 {
     public string itemId;

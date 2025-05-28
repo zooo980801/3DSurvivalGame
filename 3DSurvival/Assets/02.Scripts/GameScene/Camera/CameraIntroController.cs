243e9592dbed99f46575911132a66bc45cceb68d
zooo980801 <zooo980801@gmail.com>
2025-05-28 16:27:26 +0900

    Feat_MenuBtn

--- 3DSurvival/Assets/02.Scripts/GameScene/Camera/CameraIntroController.cs
+++ 3DSurvival/Assets/02.Scripts/GameScene/Camera/CameraIntroController.cs
@@ -1,16 +1,22 @@
-﻿using UnityEngine;
-using UnityEngine.Playables;
+﻿using UnityEngine.Playables;
+using UnityEngine;
 
 public class CameraIntroController : MonoBehaviour
 {
     public PlayableDirector director;
     public GameObject player;
     public GameObject uiRoot;
+    public GameObject menuButton;
+
+    public static bool IsIntroPlaying { get; private set; } = true;
 
     void Start()
     {
         player.SetActive(false);
         uiRoot.SetActive(false);
+        menuButton.SetActive(false);
+
+        IsIntroPlaying = true;
 
         director.stopped += OnFinished;
         director.Play();
@@ -18,7 +24,9 @@ public class CameraIntroController : MonoBehaviour
 
     void OnFinished(PlayableDirector d)
     {
+        IsIntroPlaying = false;
         player.SetActive(true);
         uiRoot.SetActive(true);
+        menuButton.SetActive(true);
     }
 }

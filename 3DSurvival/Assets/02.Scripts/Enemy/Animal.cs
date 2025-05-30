742b31213694611b115c94c5e1ef52d2fb80a6ac
YBdhhh <97926god@gmail.com>
2025-05-30 20:25:36 +0900

    feat: 동물 소환

---
+++ 3DSurvival/Assets/02.Scripts/Enemy/Animal.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour, IDamagable
+{
+    public StatusData hp;
+    public ItemData[] dropOnDeath;
+    public SpawnAnimal spawnAnimal;
+
+    public void TakePhysicalDamage(int damage)
+    {
+        hp.Subtract(damage);
+        Debug.Log("동물 아야");
+        //onTakeDamage?.Invoke();     // 데미지를 받았다는 이벤트 발생
+        //반대방향으로 도망치기
+    }
+
+    public void Die()
+    {
+        if (dropOnDeath != null)
+        {
+            for (int i = 0; i > dropOnDeath.Length; i++)
+            {
+                Instantiate(dropOnDeath[i]);
+                Debug.Log($"drop {i}");
+            }
+        }
+        spawnAnimal.spawnedAnimals.Remove(gameObject);
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp.CurValue <= 0)
+        {
+            Debug.Log("동물 피빵");
+            Die();
+        }
+    }
+}
---
+++ 3DSurvival/Assets/02.Scripts/Enemy/SpawnAnimal.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class SpawnAnimal : MonoBehaviour
+{
+    public List<GameObject> animalPrefabs;
+    public List<GameObject> spawnedAnimals;
+    public int maxAnimalCount = 50;
+    public Transform spawnCenter;       //원 중심 위치(플레이어 집)
+    public Terrain terrain;
+    public float innerRadius = 20f;     //생성 안할 안쪽 원 반지름 길이 (울타리 생기면 증가?)
+    public float outerRadius = 80f;     //생성 할 바깥쪽 원 반지름 길이
+    public float spawnTime = 30f;       //임시용 소환 쿨타임
+    public float curTime = 0f;
+    private Animal controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnAnimals();
+    }
+
+    public void SpawnAnimals()
+    {
+
+        if (spawnedAnimals.Count < maxAnimalCount) //임시 조건
+        {
+            float angle = Random.Range(0f, Mathf.PI * 2f);      //랜덤 각도 0~360도(2파이)
+            float radius = Random.Range(innerRadius, outerRadius);  //랜덤 반지름길이
+
+            float x = spawnCenter.position.x + Mathf.Cos(angle) * radius;   //랜덤한 각도와 반지름으로 코사인을 이용해 x좌표 구하기
+            float z = spawnCenter.position.z + Mathf.Sin(angle) * radius;   //랜덤한 각도와 반지름으로 사인을 이용해 z좌표 구하기
+            float y = terrain.SampleHeight(new Vector3(x, 0, z)) + terrain.transform.position.y;    //터레인 x,z 좌표에 해당하는 y값(로컬좌표) + 터레인의 y값 => 월드 포지션값 구하기
+
+            Vector3 spawnSpot = new Vector3(x, y, z);
+            GameObject animal = Instantiate(animalPrefabs[0], spawnSpot, Quaternion.identity);
+            controller = animal.GetComponent<Animal>();
+            spawnedAnimals.Add(animal);
+        }
+    }
+}
